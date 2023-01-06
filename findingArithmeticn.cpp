b2671084ae13c5dcc466b462a63e30ec9f3dbe27
Astillua007 <hritik.nagpure2013@gmail.com>
2023-01-06 09:39:16 +0530

    Finding the nth Arithmetic Term

---
+++ findingArithmeticn.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int A1=2,A2=3,N=4 ;
+    int d=A2-A1;
+    // cout<<"Enter A1 and A2 Number"<<endl;
+    // cin>>A1>>A2;
+    // cout<<"Enter the Nth Term"<<endl;
+    // cin>>N;
+    int x=A1+((N-1)*d);
+    cout<<x;
+}
