5bb45c6c1cf804917a78986be5ef41e7ddca4f85
Astillua007 <hritik.nagpure2013@gmail.com>
2023-01-07 10:06:24 +0530

    Taking Input and Printing

---
+++ takinfInputandPrinting.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    string b;
+    cout<<"Enter the integer and Number"<<endl;
+    cin>>a>>b;
+    cout<<a<<" "<<b;
+    // return 0;
+}
