f1ccb40fc33d4565f92d4edafca11a2b945170df
Astillua007 <hritik.nagpure2013@gmail.com>
2023-01-08 19:01:16 +0530

    without vector with simple for loop

---
+++ vectorTable.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+    int main(){
+        int n,v=1;
+        cout<<"Enter the number to get the Multiplication Table"<<endl;
+        cin>>n;
+        for(int i=1;i<=10;i++){
+            v=n*i;
+            cout<<v<<endl;
+        }
+        
+    }
